ae0eaa6d8e3e0f28182c115d96d06aa1ddafe9e4
Robotic-ui <84217158+Robotic-ui@users.noreply.github.com>
2021-06-16 21:05:32 -0400

    Add files via upload

---
+++ Login.py
@@ -0,0 +1,31 @@
+# this is a really simple login form run in the command line 
+# feel free to add or remove anything you want!
+
+print('Welcome to this simple login form') 
+start = input("Do You Want to Continue (y/n)? ")
+
+if start.lower() != "y":
+    print('[-] exiting...')
+    quit()
+
+loginI = input("Please Enter your Username: ")
+loginI = input("Please Enter your Password: ")
+
+login = 'admin'
+passwrd = 'password'
+
+# checks if username and password is valid
+if login.lower() != "admin":
+    login = input("Login correct, Are you sure you would like to continue(y/n)? ")
+else: 
+    print("Wrong username and/or password!")
+    quit()
+
+if passwrd.lower() != "password":
+    print("Password correct!")
+    print("yay you made it too the end!")
+else:
+    print("Password incorrect!")
+    print("oops that was incorrect Try running this program again...")
+    quit()
+
---
+++ todo.md
@@ -0,0 +1,5 @@
+HERE YOU CAN SET YOUR GOALS FOR THIS PAGE...
+make simple login page - COMPLETE 
+improve login system (make password field hidden) - IN PROGRESS <- You can do this ;) I belive in you!!
+
+
