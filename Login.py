8b60a73517e0e305d9c27b6aecb4d263f01d6c3e
Robotic-ui <84217158+Robotic-ui@users.noreply.github.com>
2021-06-30 01:40:26 -0400

    var issue now fixed :)

--- Login.py
+++ Login.py
@@ -1,27 +1,30 @@
 # this is a really simple login form run in the command line 
 # feel free to add or remove anything you want!
 
-print('Welcome to this simple login form') 
+import time 
+
+print("Welcome to this simple login form") 
 start = input("Do You Want to Continue (y/n)? ")
+time.sleep(0.6)
 
 if start.lower() != "y":
     print('[-] exiting...')
     quit()
 
+login = 'admin'
+passwrd = 'password' 
+
 loginI = input("Please Enter your Username: ")
 loginI = input("Please Enter your Password: ")
 
-login = 'admin'
-passwrd = 'password'
-
 # checks if username and password are valid
-if login.lower() != "admin":
+if login.lower() == 'admin':
     login = input("Login correct, Are you sure you would like to continue(y/n)? ")
 else: 
     print("Wrong username and/or password!")
     quit()
 
-if passwrd.lower() != "password":
+if passwrd.lower() == 'password':
     print("Password correct!")
     print("yay you made it too the end!")
 else:
